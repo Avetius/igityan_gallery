31af77ff20dacd2169d859ad46508de9a0c41464
avetius <avet.sargsyan@gmail.com>
2024-08-08 17:33:06 -0700

    files changed

---
+++ src/views/ContactView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contact">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome Contact App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'ContactView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="gallery">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome Gallery App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'GalleryView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
